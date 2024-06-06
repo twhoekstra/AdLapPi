7a011a89567bcba5f2d9a68b1c266e5b5da06502
twhoekstra <t.w.hoekstra@student.tudelft.nl>
2024-06-06 15:54:06 +0200

    Tuning

--- src/main.py
+++ src/main.py
@@ -100,7 +100,7 @@ def main(debug=True):
             if np.any(armvel != ZEROPOSITION):
                 send_serial(arduino, gcode.move(vector=armpos, order="xyz", speed=SPEED))
                 # send_serial(arduino, gcode.relative_positioning())
-                time.sleep(0.01)
+                time.sleep(0.025)
 
         # pos.clear()
         v = pos.as_array()
